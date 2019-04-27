6de5f92c394cc83ba2b669ecd9caa5b098b2190b
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-27 18:34:05 -0400

    Configure autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function compileSass () {
   return gulp.src('scss/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('css/'));
 }
 
