f3203a19019c6e501fb311523e51debf52090785
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-05-12 07:00:56 -0400

    Implement code using class

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,80 @@
-const introStatusHook = 'js-intro__status'
-const schedule = {
-  openFrom: 1, // Monday
-  openUntil: 7, // Saturday
-  openAt: 8,
-  closeAt: 18
-}
 
-const setOpen = element => {
-  if (element.classList.contains('intro__status--open')) return;
+class StatusScheduler {
+  constructor (element, schedule, { openClass, closedClass }) {
+    this.element = element || document.getElementsByClassName('status')
+    this.openClass = openClass || 'status--open'
+    this.closedClass = closedClass || 'status--closed'
+    this.schedule = schedule ||
+      { openFrom: 0, openUntil: 7, openAt: 8, closeAt: 18 }
+  }
 
-  element.classList.remove('intro__status--closed')
-  element.classList.add('intro__status--open')
-  element.setAttribute('title', 'Open')
-}
+  setOpen () {
+    if (this.element.classList.contains(this.openClass)) return
 
-const setClosed = element => {
-  if (element.classList.contains('intro__status--closed')) return;
+    this.element.classList.remove(this.closedClass)
+    this.element.classList.add(this.openClass)
+    this.element.setAttribute('title', 'Open')
+  }
 
-  element.classList.remove('intro__status--open')
-  element.classList.add('intro__status--closed')
-  element.setAttribute('title', 'Closed')
-}
+  setClosed () {
+    if (this.element.classList.contains(this.closedClass)) return
 
-const isOnSchedule = () => {
-  const currentDate = new Date()
-  const currentWeekday = currentDate.getDay()
-  const currentHour = currentDate.getHours()
+    this.element.classList.remove(this.openClass)
+    this.element.classList.add(this.closedClass)
+    this.element.setAttribute('title', 'Closed')
+  }
 
-  const inOpenDays = currentWeekday >= schedule.openFrom &&
-    currentWeekday <= schedule.openUntil
-  const inOpenHours = currentHour >= schedule.openAt &&
-    currentHour < schedule.closeAt
+  isOnSchedule () {
+    const currentDate = new Date()
+    const currentWeekday = currentDate.getDay()
+    const currentHour = currentDate.getHours()
 
-  return inOpenDays && inOpenHours
-}
+    const inOpenDays = currentWeekday >= this.schedule.openFrom &&
+      currentWeekday <= this.schedule.openUntil
+    const inOpenHours = currentHour >= this.schedule.openAt &&
+      currentHour < this.schedule.closeAt
 
-const checkStatus = element => {
-  if (isOnSchedule()) {
-    setOpen(element)
-  } else {
-    setClosed(element)
+    return inOpenDays && inOpenHours
+  }
+
+  updateStatus () {
+    if (this.isOnSchedule()) {
+      this.setOpen()
+    } else {
+      this.setClosed()
+    }
+  }
+
+  start () {
+    if (this.interval) return
+
+    this.interval = window.setInterval(() => {
+      this.updateStatus()
+    }, 1000);
+  }
+
+  stop () {
+    if (!this.interval) return
+
+    window.clearInterval(this.interval)
   }
 }
 
-const scheduleOpenStatus = () => {
+const scheduleStatus = () => {
+  const introStatusHook = 'js-intro__status'
   const introStatusEl = document.getElementsByClassName(introStatusHook)[0]
+  const schedule = {
+    openFrom: 1, // Monday
+    openUntil: 7, // Saturday
+    openAt: 7,
+    closeAt: 18
+  }
+  const scheduler = new StatusScheduler(introStatusEl, schedule, {
+    openClass: 'intro__status--open',
+    closedClass: 'intro__status--closed'
+  })
 
-  window.setInterval(function(){
-    checkStatus(introStatusEl)
-  }, 1000);
-
+  scheduler.start()
 }
 
-document.addEventListener("DOMContentLoaded", scheduleOpenStatus);
+document.addEventListener("DOMContentLoaded", scheduleStatus)
