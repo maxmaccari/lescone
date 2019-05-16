0d3dfaa3c34ed6eb50a3e13322e46856cce0b4df
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-05-16 18:42:54 -0400

    Insert javascript file on gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+const buildJavascript = () => {
+  return gulp.src('src/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js/'))
+    .pipe(browserSync.stream());
+};
 
 const buildStyles = () => {
   return gulp.src('src/scss/**/*.scss')
@@ -37,16 +45,18 @@ function server () {
 };
 
 function watch () {
+  gulp.watch('src/scss/**/*.js', buildJavascript);
   gulp.watch('src/scss/**/*.scss', buildStyles);
   gulp.watch('src/img/**/*', minifyImages);
   gulp.watch('src/static/**/*', copyStatic);
 };
 
 // Tasks
+gulp.task('javascript', buildJavascript);
 gulp.task('styles', buildStyles);
 gulp.task('images', minifyImages);
 gulp.task('static', copyStatic);
-gulp.task('build', gulp.parallel('styles', 'static', 'images'));
+gulp.task('build', gulp.parallel('javascript', 'styles', 'static', 'images'));
 gulp.task('watch', watch);
 gulp.task('server', server);
 gulp.task('default', gulp.parallel('build', 'watch', 'server'));
